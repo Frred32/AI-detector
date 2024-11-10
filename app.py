5c8297b64a02e70c04e3900d5e90b111e898e4e1
Frred32 <frredrrttt@gmail.com>
2024-11-10 13:27:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def predict_api():
         return jsonify({"error": "Prediction failed"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
